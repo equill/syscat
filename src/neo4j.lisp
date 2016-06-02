7b4b5a5b16f66ce48c753c0344fa26cc480a9065
James Fleming <james@electronic-quill.net>
2016-06-02 09:31:40 +0200

    Improve handling of empty responses
    extract-data-from-get-request now returns NIL if the response contained
    an empty data section, instead of trying to extract row-data from it.
    
    get-ipv4-address is now correspondingly smarter about receiving NIL when
    it asks for an address that isn't there.

--- src/neo4j.lisp
+++ src/neo4j.lisp
@@ -8,19 +8,21 @@
 (defun extract-data-from-get-request (response)
   "Reach into the copious data returned by a get-x request,
    and return only the actual content."
-  ;; The row data, ignoring the metadata
-  (car
-    (cdr
-      (assoc :row
-             (car
-               ;; Only the data section, ignoring the columns list
-               (cdr
-                 (assoc :data
-                        (car
-                          ;; Just the results section
-                          (cdr (assoc :results
-                                      ;; The actual query
-                                      response))))))))))
+  ;; Extract the data portion of the response
+  (let ((data (cdr
+                (assoc :data
+                       (car
+                         ;; Just the results section
+                         (cdr (assoc :results
+                                     ;; The actual query
+                                     response)))))))
+    ;; If there actually is data to be inspected, extract just the row portion.
+    ;; If there isn't, return NIL.
+    (when data
+      (car
+        (cdr
+          (assoc :row
+                 (car data)))))))
 
 
 ;;; Methods - implementing the generics
@@ -39,21 +41,20 @@
             ((:address . ,(format nil "~A" (cl-cidr-notation:parse-ip address))))))))))))
 
 (defmethod get-ipv4-address ((endpoint neo4cl:neo4j-rest-server) (address string))
-  ;; Assert which path we want to take in the event of an error
-  (handler-bind
-    ;; For now, I just want to see what went wrong
-    ((neo4cl:neo4j-transaction-error #'(lambda (e) (invoke-restart 'report-error e))))
-    ;; The thing we actually wanted to do
-    ;; Extract just the data, ignoring everything else
-    (let* ((response-data
-             (extract-data-from-get-request
-               (neo4cl:neo4j-transaction
-                 endpoint
-                 `((:STATEMENTS
-                     ((:STATEMENT . ,(format nil "MATCH (i:ipv4Addr {address: '~A'}) RETURN i"
-                                             (cl-cidr-notation:parse-ip address)))))))))
-           (address (cdr (assoc :address response-data))))
-      ;; Convert it back to dotted-quad notation before returning it.
+  ;; Extract just the data, ignoring everything else
+  (let* ((response-data
+           (extract-data-from-get-request
+             (neo4cl:neo4j-transaction
+               endpoint
+               `((:STATEMENTS
+                   ((:STATEMENT . ,(format nil "MATCH (i:ipv4Addr {address: '~A'}) RETURN i"
+                                           (cl-cidr-notation:parse-ip address)))))))))
+         ;; Extract the address from the response, but only if
+         ;; there's something to inspect
+         (address (when response-data
+                    (cdr (assoc :address response-data)))))
+    ;; If an address was returned, convert it back to dotted-quad notation
+    (when address
       (cl-cidr-notation:ip-string (parse-integer address)))))
 
 (defmethod delete-ipv4-address ((endpoint neo4cl:neo4j-rest-server) (address string))
