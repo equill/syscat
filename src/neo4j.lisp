f5ff42d75a9e466de3618bf2f55fdecaa32e9876
James Fleming <james@electronic-quill.net>
2016-06-15 08:48:08 +0200

    Return all data from get-ipv4-address
    Instead of extracting one field from the data returned by Neo4J, return
    all of it.

--- src/neo4j.lisp
+++ src/neo4j.lisp
@@ -21,19 +21,12 @@
 
 (defmethod get-ipv4-address ((endpoint neo4cl:neo4j-rest-server) (address string))
   ;; Extract just the data, ignoring everything else
-  (let* ((response-data
-           (neo4cl:extract-data-from-get-request
-             (neo4cl:neo4j-transaction
-               endpoint
-               `((:STATEMENTS
-                   ((:STATEMENT . ,(format nil "MATCH (i:ipv4Addr {address: '~A'}) RETURN i"
-                                           (canonicalise-ipv4-addr address)))))))))
-         ;; Extract the address from the response, but only if
-         ;; there's something to inspect
-         (returned-address (when response-data
-                    (cdr (assoc :address response-data)))))
-    ;; If an address was returned, convert it back to dotted-quad notation
-    (when returned-address (canonicalise-ipv4-addr returned-address))))
+  (neo4cl:extract-data-from-get-request
+    (neo4cl:neo4j-transaction
+      endpoint
+      `((:STATEMENTS
+          ((:STATEMENT . ,(format nil "MATCH (i:ipv4Addr {address: '~A'}) RETURN i"
+                                  (canonicalise-ipv4-addr address)))))))))
 
 (defmethod delete-ipv4-address ((endpoint neo4cl:neo4j-rest-server) (address string))
   (neo4cl:neo4j-transaction
