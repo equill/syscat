88885addd122fe8435a12ce110de8271477459c4
James Fleming <james@electronic-quill.net>
2017-01-13 23:19:31 +0100

    06 Add stub for test script

---
+++ test/demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import requests
+
+URL='http://localhost:4950/'
+REST='%sapi/v1' % URL
+
+requests.post('%s/deviceRoles' % REST, data={'uid': 'router'})
+requests.get('%s/deviceRoles' % REST)
+requests.post('%s/devices' % REST, data={'uid': 'amchitka'})
+requests.get('%s/devices' % REST)
