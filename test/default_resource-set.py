2518865f1408da30dae60071498cbc0bbc257d60
James Fleming <james@electronic-quill.net>
2017-02-01 21:53:39 +1100

    Add more sanity to the default schema and resources
    Addresses #6

--- test/default_resource-set.py
+++ test/default_resource-set.py
@@ -5,20 +5,25 @@ import requests
 URL='http://localhost:4950'
 REST='%s/raw/v1' % URL
 
+def post(uri, data):
+    response = requests.post('%s/%s' % (REST, uri), data=data)
+    print '%s: %s' % (uri, response.status_code)
+
 # Us
-requests.post('%s/asn' % REST , data={'uid': 'default', 'name': 'Default ASN (us)'})
+post('asn', {'uid': 'default', 'name': 'Default ASN (us)'})
+post('organisations', {'uid': 'us', 'description': 'Our own organisation.'})
 
 # The Unknown / the Other
-requests.post('%s/asn' % REST , data={'uid': 'unknown', 'name': 'Somebody else'})
-requests.post('%s/devices' % REST, data={'uid': 'unknown'})
-requests.post('%s/devices/unknown/Interfaces/' % REST, data={'type': 'interfaces', 'uid': 'unknown'})
-requests.post('%s/devices/unknown/MemberOf/' % REST, data={'target': '/asn/unknown'})
+post('devices', {'uid': 'unknown'})
+post('devices/unknown/Interfaces/', {'type': 'interfaces', 'uid': 'unknown'})
+post('organisations', {'uid': 'unknown', 'description': 'Unknown organisation. Not us.'})
+post('devices/unknown/BusinessOwner/', {'target': '/organisations/unknown'})
 
 # The Internet
-requests.post('%s/asn' % REST , data={'uid': 'internet', 'name': 'The Internet'})
-requests.post('%s/asn/internet/subnets' % REST, data={'type': 'ipv4Addresses', 'uid': '0.0.0.0'})
+post('asn', {'uid': 'internet', 'name': 'The Internet'})
+post('asn/internet/Subnets', {'type': 'ipv4Subnets', 'uid': '0.0.0.0/0'})
 
 # Generic stuff
-requests.post('%s/deviceRoles' % REST, data={'uid': 'router'})
-requests.post('%s/deviceRoles' % REST, data={'uid': 'switch'})
-requests.post('%s/deviceRoles' % REST, data={'uid': 'host'})
+post('deviceRoles', {'uid': 'router'})
+post('deviceRoles', {'uid': 'switch'})
+post('deviceRoles', {'uid': 'host'})
