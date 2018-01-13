a66a81112466362b12f0ce1742ed888858cb5ca3
James Fleming <james@electronic-quill.net>
2018-01-13 14:26:56 +0100

    Add create_executable.sh, to enable dockerisation

---
+++ scripts/create_executable.sh
@@ -0,0 +1,3 @@
+/usr/local/bin/sbcl --userinit sbclrc-docker \
+    --eval "(asdf:load-system :syscat)" \
+    --eval "(sb-ext:save-lisp-and-die \"syscat\" :executable t :toplevel #'syscat::dockerstart)"
